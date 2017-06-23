8e5f2bfd1e836ed32746a4babb4827e34a4954a0
AIE\s171679 <alyssafackrell@gmail.com>
2017-06-23 14:02:06 +1000

    Added Spawn volume class which will spawn random object withing a bounding area on a timer basis

---
+++ Source/BatteryCollector/SpawnVolume.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BatteryCollector.h"
+#include "SpawnVolume.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Pickup.h"
+
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	//Create default box component
+	m_whereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("WhereToSpawn"));
+	RootComponent = m_whereToSpawn;
+
+	//Set spawn delay range
+	spawnDelayMin = 1.0f;
+	spawnDelayMax = 4.5f;
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	m_spawnDelay = FMath::FRandRange(spawnDelayMin, spawnDelayMax);
+	GetWorldTimerManager().SetTimer(spawnTimer, this, &ASpawnVolume::SpawnPickup, m_spawnDelay, false);
+
+}
+
+// Called every frame
+void ASpawnVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume() {
+
+	FVector spawnOrigin = m_whereToSpawn->Bounds.Origin;
+	FVector spawnExtents = m_whereToSpawn->Bounds.BoxExtent;
+
+	return UKismetMathLibrary::RandomPointInBoundingBox(spawnOrigin, spawnExtents);
+
+}
+
+void ASpawnVolume::SpawnPickup() {
+
+	//If we have set something to spawn
+	if(whatToSpawn != NULL) {
+		//Check for valid world
+		UWorld* const world = GetWorld();
+
+		if(world) {
+
+			//Set the spawn parameters
+			FActorSpawnParameters spawnParams;
+			spawnParams.Owner = this;
+			spawnParams.Instigator = Instigator;
+
+			//Get a random location to spawn at
+			FVector spawnLocation = GetRandomPointInVolume();
+
+			//Get a random rotation
+			FRotator spawnRotation;
+			spawnRotation.Yaw = FMath::FRand() * 360.0f;
+			spawnRotation.Pitch = FMath::FRand() * 360.0f;
+			spawnRotation.Roll = FMath::FRand() * 360.0f;
+
+			//Spawn the pickup
+			APickup* const spawnedPickup = world->SpawnActor<APickup>(whatToSpawn, spawnLocation, spawnRotation, spawnParams);
+
+			m_spawnDelay = FMath::FRandRange(spawnDelayMin, spawnDelayMax);
+			GetWorldTimerManager().SetTimer(spawnTimer, this, &ASpawnVolume::SpawnPickup, m_spawnDelay, false);
+
+		}
+
+	}
+
+}
+
---
+++ Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FORCEINLINE UBoxComponent* GetWhereToSpawn() const { return m_whereToSpawn; }
+
+	//Find random point in box bounds
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetRandomPointInVolume();
+
+protected:
+	//The pickup to spawn
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class APickup> whatToSpawn;
+
+	FTimerHandle spawnTimer;
+
+	//Min spawn delay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float spawnDelayMin;
+
+	//Max spawn delay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float spawnDelayMax;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* m_whereToSpawn;
+
+	//Handles Pickup spawns
+	void SpawnPickup();
+
+	//Actual spawn delay
+	float m_spawnDelay;
+	
+};
